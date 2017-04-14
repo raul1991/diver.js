cdb6d086b2c3a7bb099adb69738b227844924237
raul1991 <supremeanitabawa@gmail.com>
2017-04-14 22:33:38 +0530

    upgraded to gulp4.0 alpha release. Tasks are now private.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
-var gulp = require('gulp');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
+var gulp = require("gulp"),
+	uglify = require("gulp-uglify"),
+	rename = require("gulp-rename"),
+	jshint = require("gulp-jshint"),
+	sequence = require("gulp-sequence"),
+	del = require("del"),
+	watch = require("gulp-watch");
 
-gulp.task('build', function() {
- gulp.src('diver.js')
-     .pipe(gulp.dest('dist/'))
-     .pipe(rename('diver.min.js')) 
-     .pipe(uglify())
-     .pipe(gulp.dest('dist'))
-});
+function package() {
+	return gulp.src("diver.js")
+		.pipe(gulp.dest("dist"));
+}
+
+function minify() {
+	return gulp.src('diver.js')
+		.pipe(gulp.dest('dist'))
+		.pipe(rename('diver.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+
+}
+
+function lint() {
+	return gulp.src("**/*.js")
+		.pipe(jshint())
+		.pipe(jshint.reporter());
+
+}
+
+function clean() {
+	return del([
+		"dist/**/*"
+	]);
+}
+
+var build = gulp.series(clean, lint, minify, package);
+gulp.task('build', build);
