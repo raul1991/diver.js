0d850527f0407930108adf455ba05d5fc3f81c6c
Dan Gilkerson <contact@dangilkerson.com>
2019-10-03 09:01:34 -0700

    Example specs

--- tests/helper.js
+++ tests/helper.js
@@ -1,7 +1,14 @@
+
 function verifyDiver(html, expected, id) {
+  let existingTestContainer = document.getElementById("diver-test");
+  if (existingTestContainer) {
+    document.body.removeChild(existingTestContainer);
+  }
+
   const testContainer = document.createElement("div");
   testContainer.setAttribute("id", "diver-test");
   testContainer.insertAdjacentHTML("afterbegin", html);
-
+  document.body.appendChild(testContainer);
+  
   expect(diver.traverse(id || testContainer, {})).toEqual(expected);
 }
